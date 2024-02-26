7281db52f4d454d420ddc9b22521c5da363a4adb
Flavio Silva <fal_silva@hotmail.com>
2024-02-26 11:03:44 -0300

    configurações e demonstrações gerais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
